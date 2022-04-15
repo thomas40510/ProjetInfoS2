df68f3628f1be9e0f906d15e38d36085a5a4de9a
nicod73 <102388478+nicod73@users.noreply.github.com>
2022-04-15 12:46:40 +0200

    Add files via upload
    Version fonctionnelle de mon côté, manque joueur humain + interface humain

--- test_Tarot_Africain.py
+++ test_Tarot_Africain.py
@@ -4,48 +4,48 @@ from Tarot_Africain import *
 
 
 class Test_Tarot_Africain(unittest.TestCase):
-    def testcoherenceattributioncartes(self):
-        for nombredejoueurs in range(2, 5):
-            for nombredecartes in range(1, 5):
+    def testcohérenceattributioncartes(self):
+        for nombredejoueurs in range(2,5):
+            for nombredecartes in range(1,5):
                 for nombredetests in range(20):
-                    N = [[str(k), str(k)] for k in range(nombredejoueurs)]
-                    M = Manche(N, nombredecartes, 0, False)
-                    L = []
+                    N=[[str(k),str(k)] for k in range(nombredejoueurs)]
+                    M=Manche(N,nombredecartes,0,False)
+                    L=[]
                     for joueur in M.joueurs:
                         for k in joueur.cartes:
                             L.append(k)
-                    coh = True
-                    LP = []
+                    coh=True
+                    LP=[]
                     for k in L:
                         if k not in LP:
                             LP.append(k)
                         elif k in LP:
-                            coh = False
+                            coh=False
                     self.assertTrue(coh)
 
-    def testcoherencespari(self):
-        for nombredejoueurs in range(2, 5):
-            for nombredecartes in range(1, 5):
+    def testcohérencespari(self):
+        for nombredejoueurs in range(2,5):
+            for nombredecartes in range(1,5):
                 for nombredetests in range(20):
                     N = [[str(k), str(k)] for k in range(nombredejoueurs)]
                     M = Manche(N, nombredecartes, 0, False)
-                    L = M.paris()
-                    self.assertNotEqual(sum(L), nombredecartes)
+                    L=M.paris()
+                    self.assertNotEqual(sum(L),nombredecartes)
                     for k in L:
                         self.assertTrue(k >= 0)
-                        self.assertTrue(k <= nombredecartes)
+                        self.assertTrue(k<=nombredecartes)
+
 
     def testpertespoints(self):
-        for nombredejoueurs in range(2, 5):
-            for nombredecartes in range(1, 5):
+        for nombredejoueurs in range(2,5):
+            for nombredecartes in range(1,5):
                 for nombredetests in range(20):
                     N = [[str(k), str(k)] for k in range(nombredejoueurs)]
                     M = Manche(N, nombredecartes, 0, False)
-                    P = M.jeu
-                    self.assertNotEqual(P, [0 for k in range((nombredejoueurs))])
+                    P=M.jeu
+                    self.assertNotEqual(P,[0 for k in range((nombredejoueurs))])
 
 
-# unittest.main()
 
-if __name__ == '__main__':
-    unittest.main()
+
+unittest.main()
